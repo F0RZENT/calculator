0b8de3b794bd6e7bd134092ecbd0a904c8f95f06
F0RZENT <113331099+F0RZENT@users.noreply.github.com>
2023-10-10 23:34:35 +0500

    Add files via upload

---
+++ 123/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Сalculator {
+    public static void Main(){
+    
+        Console.WriteLine("Введите первое число:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите оператор:");
+        char oper = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+  Сalculator num = new Сalculator();
+        if (oper == '+'){
+          Console.WriteLine("Равняется:" + (a + b));
+        }
+ 
+         if (oper == '-'){
+        Console.WriteLine("Равняется:" + (a - b));
+        }
+ 
+          if (oper == '*')
+        {
+        Console.WriteLine("Равняется:" + (a * b));
+        }
+ 
+         if (oper == '/')
+        {
+        Console.WriteLine("Равняется:" + (a / b));
+        }
+      }
+    }
